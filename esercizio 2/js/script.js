2b2dc5dee5ee2748459889bcbba2fc170996de07
BartCatalano <catalanobartolomeo@gmail.com>
2024-11-12 17:28:34 +0100

    feat: secondo esercizio fatto, i nomi dei team sono bellissimi <3

--- esercizio 2/js/script.js
+++ esercizio 2/js/script.js
@@ -6,8 +6,29 @@ const Squadre = [
     { nome: `Spiacenza`, punti: 0, fallisubiti: 0}, 
 ];
 
-function numeriCasuali(max, min) {
-    matchMedia.random
+// CREO due NUMERI RANDOM nelle var per punti e falli
+//li inserisco  nelle rispettive chiavi dell'oggetto 
+
+
+    for (let i = 0; i < Squadre.length; i++) {
+        const curTeam = Squadre[i];
+        const numeroPunti = Math.floor(Math.random() * 100); // Numeri casuali tra 1 e 100
+        const numeroFalli = Math.floor(Math.random() * 100); // Numeri casuali tra 1 e 100
+       curTeam.punti = numeroPunti;
+       curTeam.fallisubiti = numeroFalli;
+    }
+    console.log(Squadre);
+// creo nuovo array e divido i dati
+    const arrayNomiFalli= [];
+    const arrayPunti = [];
+
+    for(let i = 0; i < Squadre.length; i++){
+        const curSquadra = Squadre[i];
+            arrayNomiFalli.push(curSquadra.nome);
+            arrayNomiFalli.push(curSquadra.fallisubiti);
+            arrayPunti.push(curSquadra.punti);
+    }
     
-}
+console.log(arrayNomiFalli,arrayPunti);
+
 
