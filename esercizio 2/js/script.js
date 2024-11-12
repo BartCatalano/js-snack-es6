8a2679bb2a65ece78f58268a197415b831c467db
BartCatalano <catalanobartolomeo@gmail.com>
2024-11-12 16:54:16 +0100

    feat: esercizio uno finito, esercizio 2 (WIP)

---
+++ esercizio 2/js/script.js
@@ -0,0 +1,13 @@
+// Creo array di team con solo il nome inserito
+
+const Squadre = [
+    { nome: `CeltaVino`, punti: 0, fallisubiti: 0},
+    { nome: `RealMadrink`, punti: 0, fallisubiti: 0},
+    { nome: `Spiacenza`, punti: 0, fallisubiti: 0}, 
+];
+
+function numeriCasuali(max, min) {
+    matchMedia.random
+    
+}
+
